47e2c33d44661f24d3ef35f0f8b6741e36cfa54a
yejiming <415386365@qq.com>
2024-10-13 22:42:50 +0800

    11

--- contracts/FundMe/FundMe.sol
+++ contracts/FundMe/FundMe.sol
@@ -1,3 +1,14 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.26;
+pragma solidity 0.8.24;
+
+contract FundMe {
+
+    uint256 minimumUsd = 5;
+
+    function fund() public payable {
+        require(msg.value >= minimumUsd, "didn't send enough ETH");
+    }
+
+    // function withdraw() public {}
+}
--- contracts/SimpleStorage/StorageFactory.sol
+++ contracts/SimpleStorage/StorageFactory.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.19;
+pragma solidity 0.8.24;
 
 // import {SimpleStorage, SimpleStorage2} from "./SimpleStorage.sol";
 import {SimpleStorage} from "./SimpleStorage.sol";
